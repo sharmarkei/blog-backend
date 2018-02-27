e506293312bf03d004776a4884c2d76e976bb9df
sharmarke <smarky292@gmail.com>
2018-02-27 14:38:57 -0800

    created user model

---
+++ src/model/user.js
@@ -0,0 +1,79 @@
+'use strict';
+
+const crypto = require('crypto'); // for getting random strnig to to be tokenHash
+const bcrypt = require('bcrypt'); // for hashing password
+const jwt = require('jsonwebtoken'); // for encrypting tokenHash to create token
+const mongoose = require('mongoose');
+const createError = require('http-errors');
+const Promise = require('bluebird');
+const debug = require('debug')('blog:user');
+
+const Schema = mongoose.Schema;
+
+const userSchema = Schema({
+  username: { type: String, required: true, unique: true },
+  email: { tpye: String, required: true, unique: true },
+  password: { type: String, required: true },
+  tokenHash: { type: String, unique: true }
+});
+
+//Takes a plain password and hashes it using bcrypt, stores on user model
+
+userSchema.methods.generatePasswordHash = function (password) {
+  debug('generatePasswordHash');
+
+  return new Promise((resolve, reject) => {
+    bcrypt.hash(password, 8, (err, hash) => {
+      if (err) return reject(err);
+      this.password = hash;
+      resolve(this);
+    });
+  });
+};
+
+//takes a plain password and compares it to the hashed password on user model
+
+userSchema.methods.comparePasswordHash = function (password) {
+  debug('comparePasswordHash');
+
+  return new Promise((resolve, reject) => {
+    bcrypt.compare(password, this.password, (err, valid) => {
+      if (err) return reject(err);
+      if (!valid) return reject(createError(401, 'INVALID PASSWORD'));
+      resolve(this);
+    });
+  });
+};
+
+userSchema.methods.generateFindHash = function () {
+  debug('generateFindHash');
+
+  return new Promise((resolve, reject) => {
+    let tries = 0;
+
+    _generateFindHash.call(this);
+
+    function _generateFindHash() {
+      this.findHash = crypto.randomBytes(32).toString('hex');
+      this.save()
+        .then(() => resolve(this.findHash))
+        .catch(err => {
+          if (tries > 3) return reject(err);
+          tries++;
+          _generateFindHash.call(this);
+        });
+    }
+  });
+}
+
+userSchema.methods.generateToken = function () {
+  debug('generateToken');
+
+  return new Promise((resolve, reject) => {
+    this.generateFindHash()
+      .then(findHash => resolve(jwt.sign({ token: findHash }, process.env.APP_SECRET)))
+      .catch(err => reject(err));
+  });
+}
+
+module.exports = mongoose.model('user', userSchema);
